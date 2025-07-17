7d9cbbd8e996e2927a67ea5e0cd98d68bef031f0
Kamkmgamer <khalilabdalmajeed@gmail.com>
2025-07-18 01:43:57 +0300

    chore: update About Me image with new version

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -25,7 +25,7 @@ const AboutSection: React.FC = () => {
           className="relative w-full h-80 md:h-[450px] rounded-3xl overflow-hidden shadow-xl"
         >
           <Image
-            src="https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/about-me.jpg"
+            src="https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/AboutMe.png"
             alt="Khalil Abdel Majeed working on a project"
             fill
             style={{ objectFit: 'cover' }}
