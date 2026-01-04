3d9fe478e6d1ec51a0db631ece913bcd11486447
Kamkmgamer <khalilabdalmajeed@gmail.com>
2026-01-04 23:11:27 +0200

    style: clean up projects data file formatting

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,10 +1,11 @@
+
 export interface Project {
   id: number;
   title: string;
   description: string;
   image: string;
   tags: string[];
-  gradient: string; // Tailwind gradient class
+  gradient: string;
   github?: string;
   demo?: string;
   category: string;
@@ -13,6 +14,15 @@ export interface Project {
 export const projects: Project[] = [
   {
     id: 1,
+    title: "kamkmPDF",
+    description: "A modern PDF generator using AI.",
+    image: "https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/projects/Kamkm-PDF.png",
+    tags: ["Nextjs", "Web Design", "Responsive"],
+    gradient: "from-cyan-500 via-emerald-500 to-red-500",
+    demo: "https://kamkm-pdf.netlify.app/",
+    category: "Web Design",
+  },
+    {id: 2,
     title: "SoftMedics.sd",
     description: "A modern e-learning platform for professionals in Sudan.",
     image: "https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/projects/image.png",
@@ -22,7 +32,7 @@ export const projects: Project[] = [
     category: "Web Design",
   },
   {
-    id: 2,
+    id: 3,
     title: "Blue Nile Paint",
     description: "Landing page for a paint company with CMS support.",
     image: "https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/projects/Screenshot%202025-08-04%20024500.png",
@@ -32,7 +42,7 @@ export const projects: Project[] = [
     category: "Web Design",
   },
   {
-    id: 3,
+    id: 4,
     title: "Fitness Khalil",
     description: "A fitness site designed for trainers.",
     image: "https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/projects/Screenshot%202025-08-04%20024232.png",
@@ -42,17 +52,17 @@ export const projects: Project[] = [
     category: "Web Design",
   },
   {
-    id: 4,
+    id: 5,
     title: "Kamkm-inc",
     description: "A modern company website for Kamkm-inc.",
     image: "https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/projects/Screenshot%202025-09-19%20214015.png",
     tags: ["Nextjs", "Web Design", "Responsive"],
     gradient: "from-cyan-500 via-emerald-500 to-red-500",
-    demo: "https://skamkm-inc.khalil.mageed.net/",
+    demo: "https://kamkm-inc.khalil.mageed.net/",
     category: "Web Design",
   },
   {
-    id: 5,
+    id: 6,
     title: "Kamkmserve",
     description: "A modern e-commerce platform for web services.",
     image: "https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/projects/Screenshot%202025-09-19%20214214.png",
@@ -62,7 +72,7 @@ export const projects: Project[] = [
     category: "Web Design",
   },
   {
-    id: 6,
+    id: 7,
     title: "Tanoshi-game",
     description: "A modern web game for entertainment.",
     image: "https://ik.imagekit.io/gtnmxyt2d/khalil-portfolio/projects/Screenshot%202025-09-19%20214241.png",
