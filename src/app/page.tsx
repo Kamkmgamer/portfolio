57750d0590f870c90201bd74bd72fbdad681a24d
Kamkmgamer <khalilabdalmajeed@gmail.com>
2026-01-09 01:38:54 +0200

    feat(seo): add WebPage schema to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,20 @@
 import HeroSection from "../components/HeroSection";
 import ServicesSection from "@/components/ServicesSection";
 
+import JsonLd from "@/components/seo/JsonLd";
+
 export default function Home() {
+  const jsonLdData = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    name: "Khalil Abd Almageed | Web Developer & Designer",
+    description: "Portfolio of Khalil Abd Almageed...",
+    url: "https://khalil.mageed.net",
+  };
+
   return (
     <>
+      <JsonLd data={jsonLdData} />
       <HeroSection />
       <ServicesSection />
     </>
