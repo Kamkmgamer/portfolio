b7e8ac961d04c150c3668c0dd563c356117f74b0
Kamkmgamer <khalilabdalmajeed@gmail.com>
2026-01-12 16:06:15 +0200

    fix: update crystalline shards imports

--- src/components/three/CrystallineShards.tsx
+++ src/components/three/CrystallineShards.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useRef, useMemo } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 
 interface ShardProps {
