05194497818fb1734598af968f3f7f90c2f892a8
Kamkmgamer <khalilabdalmajeed@gmail.com>
2026-02-03 21:59:25 +0200

    feat(pages): add offers page to showcase past projects

---
+++ src/app/offers/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import OfferList from "@/components/OfferList";
+import { offers } from "@/data/offers";
+
+export default function OffersPage() {
+    return (
+        <main className="min-h-screen pt-32 pb-20 px-6">
+            <div className="max-w-[1800px] mx-auto">
+                <OfferList offers={offers} />
+            </div>
+        </main>
+    );
+}
